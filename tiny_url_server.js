d320f128984f82b8da68922fa397bbae49ac7ed9
John Chow <JChow417@gmail.com>
2016-09-16 00:17:07 +0000

    Completed integration of mongodb

--- tiny_url_server.js
+++ tiny_url_server.js
@@ -1,114 +1,164 @@
 "use strict";
 
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
 //var connect = require('connect');
-var methodOverride = require('method-override');
+const methodOverride = require('method-override');
+const MongoClient = require("mongodb").MongoClient;
+const MONGODB_URI = "mongodb://127.0.0.1:27017/url_shortener";
 
-function generateRandomString() {
-  let numberOfChars = 6;
-  let alphanumericChar = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  let resultArr = [];
-  for(let i = 0; i < numberOfChars; i++) {
-    let charIndex = Math.floor(Math.random() * alphanumericChar.length);
-    let char = alphanumericChar.charAt(charIndex);
-    resultArr.push(char);
+MongoClient.connect(MONGODB_URI, (err, dbInstance) => {
+  if (err) {
+    console.log('Could not connect! Unexpected error. Details below.');
+    throw err;
   }
-  return resultArr.join("");
-}
+  console.log('Connected to the database!');
+  const collection = dbInstance.collection("urls")
 
-const bodyParser = require("body-parser");
-app.use(bodyParser.urlencoded());
-app.use(methodOverride('_method'));
-
-app.set("view engine", "ejs");
-var PORT = process.env.PORT || 8080; // default port 8080
-
-var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
-
-// app.get("/", (req, res) => {
-//   res.end("Hello!");
-// });
-
-app.get("/", (req, res) => {
-  res.redirect("urls/new");
-});
-
-app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
-  res.render('urls_index', templateVars);
-});
-
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
-app.get("/urls/:id", (req, res) => {
-  let shortURL = req.params.id;
-  if (urlDatabase.hasOwnProperty(shortURL)) {
-    let longURL = urlDatabase[shortURL]
-    let templateVars = { shortURL: shortURL, longURL: longURL};
-    res.render("urls_show", templateVars);
-  } else {
-    res.status(404);
-    res.send("SHORT URL DOES NOT EXIST");
+  function generateRandomString() {
+    let numberOfChars = 6;
+    let alphanumericChar = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let resultArr = [];
+    for(let i = 0; i < numberOfChars; i++) {
+      let charIndex = Math.floor(Math.random() * alphanumericChar.length);
+      let char = alphanumericChar.charAt(charIndex);
+      resultArr.push(char);
+    }
+    return resultArr.join("");
   }
-});
-// OTHERWISE GIVE ERROR MSG
-// REMEMBER TO HAVE STATUS CODE eg. 4XX
 
-app.post("/urls", (req, res) => {
-  let longURL = req.body.longURL;  // debug statement to see POST parameters
-  let newKey = generateRandomString();
-  while (urlDatabase.hasOwnProperty(newKey)) {
-    newKey = generateRandomString();
+  function getLongURL(db, shortURL, cb) {
+    let query = { "shortURL": shortURL };
+    collection.findOne(query, (err, result) => {
+      if (err) {
+        return cb(err);
+      }
+      if (result) {
+        return cb(null, result.longURL);
+      } else {
+        return cb(null, null);
+      }
+    });
   }
-  urlDatabase[newKey] = longURL;
 
-  res.redirect("/urls/" + newKey);         // Respond with 'Ok' (we will replace this)
-});
-
-app.get("/u/:shortURL", (req, res) => {
-  let urlKey = req.params.shortURL;
-  if(urlDatabase.hasOwnProperty(urlKey)) {
-    let longURL = urlDatabase[urlKey];
-    res.redirect(longURL);
-  } else {
-    res.status(404);
-    res.send("SHORT URL DOES NOT EXIST");
+  function getURLs(cb) {
+    collection.find().toArray((err, urlArray) => {
+      if(err){
+        throw err;
+      }
+      let urlDatabase = {};
+      urlArray.forEach((i) => {
+        urlDatabase[i.shortURL] = [i.longURL];
+      });
+      cb(urlDatabase);
+    });
   }
-});
-// OTHERWISE GIVE ERROR MSG
-// REMEMBER TO HAVE STATUS CODE eg. 4XX
 
-app.delete("/urls/:id", (req, res) => {
-  let id = req.params.id;
-  console.log(id);
-  delete urlDatabase[id];
-  res.redirect("/urls");
-});
+  const bodyParser = require("body-parser");
+  app.use(bodyParser.urlencoded());
+  app.use(methodOverride('_method'));
 
-app.put("/urls/:id", (req, res) => {
-  let id = req.params.id;
-  let newLongURL = req.body.newLongURL;
-  urlDatabase[id] = newLongURL;
-  console.log(newLongURL);
+  app.set("view engine", "ejs");
+  var PORT = process.env.PORT || 8080; // default port 8080
 
-  res.redirect("/urls/");
-});
+  // var urlDatabase = {
+  //   "b2xVn2": "http://www.lighthouselabs.ca",
+  //   "9sm5xK": "http://www.google.com"
+  // };
+
+  // app.get("/", (req, res) => {
+  //   res.end("Hello!");
+  // });
+
+  app.get("/", (req, res) => {
+    res.redirect("urls/new");
+  });
+
+  app.get("/urls", (req, res) => {
+    getURLs((urlDatabase) => {
+      res.render("urls_index", {urls: urlDatabase})
+    });
+  });
+
+  app.post("/urls", (req, res) => {
+    let longURL = req.body.longURL;  // debug statement to see POST parameters
+    getURLs((urlDatabase) => {
+      let newKey = generateRandomString();
+      while (urlDatabase.hasOwnProperty(newKey)) {
+         newKey = generateRandomString();
+      }
+      let urlObject = {"shortURL": newKey, "longURL": longURL};
+      collection.insert(urlObject, (err, doc) => {
+        if(err){
+          throw err;
+        }
+        res.redirect("/urls/" + newKey);         // Respond with 'Ok' (we will replace this)
+      });
+    })
+  });
+
+  app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
+
+  app.get("/urls/:id", (req, res) => {
+    let shortURL = req.params.id;
+    getLongURL(dbInstance, shortURL, (err, longURL) => {
+      if(longURL !== null) {
+        let templateVars = { shortURL: shortURL, longURL: longURL};
+        res.render("urls_show", templateVars);
+      } else {
+        res.status(404);
+        res.send("SHORT URL DOES NOT EXIST");
+      }
+    });
+  });
+  // OTHERWISE GIVE ERROR MSG
+  // REMEMBER TO HAVE STATUS CODE eg. 4XX
+
+  app.put("/urls/:id", (req, res) => {
+    let shortURL = req.params.id;
+    let newLongURL = req.body.newLongURL;
+    collection.update(
+      {'shortURL': shortURL},
+      { $set : {'longURL': newLongURL}},() => {
+      res.redirect("/urls/");
+    });
+  });
+
+  app.delete("/urls/:id", (req, res) => {
+    let shortURL = req.params.id;
+    //console.log(id);
+    collection.remove({'shortURL': shortURL},() => {
+      res.redirect("/urls");
+    });
+  });
+
+  app.get("/u/:shortURL", (req, res) => {
+    let urlKey = req.params.shortURL;
+    getURLs((urlDatabase) => {
+      if(urlDatabase.hasOwnProperty(urlKey)) {
+        let longURL = urlDatabase[urlKey];
+        res.redirect(longURL);
+      } else {
+        res.status(404);
+        res.send("SHORT URL DOES NOT EXIST");
+      }
+    });
+  });
+  // OTHERWISE GIVE ERROR MSG
+  // REMEMBER TO HAVE STATUS CODE eg. 4XX
 
 
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
+  // app.get("/urls.json", (req, res) => {
+  //   res.json(urlDatabase);
+  // });
 
-// app.get("/hello", (req, res) => {
-//   res.end("<html><body>Hello <b>World</b></body></html>\n");
-// });
+  // app.get("/hello", (req, res) => {
+  //   res.end("<html><body>Hello <b>World</b></body></html>\n");
+  // });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}!`);
+  });
 });
